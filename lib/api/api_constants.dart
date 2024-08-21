da4b5fe613f130d27b524d02df521abe8d557178
ELMAHEDY <as4778328@gmail.com>
2024-08-21 20:20:53 +0300

    Update Api constants.

--- lib/api/api_constants.dart
+++ lib/api/api_constants.dart
@@ -1,1 +1,14 @@
-class ApiConstants {}
+class ApiConstants {
+  static const String baseUrl = 'api.themoviedb.org';
+  static const String apiKay = 'fecf6be8bcaf8b29e6d94548a8dedac6';
+  /// Api popular
+  static const String popularApi = '/3/person/popular';
+  /// Api Upcoming
+  static const String upComingApi = '/3/movie/upcoming';
+  /// Api Top Rated => Recommended
+  static const String topRatedApi = '/3/movie/top_rated';
+  /// Api Details => Movie Details.
+  static const String detailsApi = '/3/movie/{movie_id}';
+  /// Api Similar => more like this section.
+  static const String similarApi = '/3/movie/{movie_id}/similar';
+}
