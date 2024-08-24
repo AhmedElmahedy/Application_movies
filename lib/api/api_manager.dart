70f16070ee5ea58b904a21582046de1289405298
ELMAHEDY <as4778328@gmail.com>
2024-08-24 21:30:18 +0300

    Edit home tab.

--- lib/api/api_manager.dart
+++ lib/api/api_manager.dart
@@ -1,1 +1,24 @@
-class ApiManager {}
+import 'dart:convert';
+import 'package:app_movies/api/api_constants.dart';
+import 'package:app_movies/model/ResponsePopularSuccess.dart';
+import 'package:http/http.dart' as http;
+
+class ApiManager {
+  /*
+  https://api.themoviedb.org/3/person/popular
+   */
+  static Future<ResponsePopularSuccess?> getPopular() async {
+    Uri url = Uri.https(ApiConstants.baseUrl, ApiConstants.popularApi,
+        {
+          'api_key' : ApiConstants.apiKay
+        });
+    try{
+      var response = await http.get(url);
+        var responseBody = response.body;
+        var json = jsonDecode(responseBody);
+        return ResponsePopularSuccess.fromJson(json);
+    }catch (e){
+      throw e ;
+    }
+  }
+}
